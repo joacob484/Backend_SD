3b4f387fb5be7edb3ea819eee55951309ba879ba
augusto-leborgne <leborgneaugusto@gmail.com>
2025-10-22 22:07:57 -0300

    test: Add version log to test zero-downtime deployment

--- src/main/java/uy/um/faltauno/FaltaUnoApplication.java
+++ src/main/java/uy/um/faltauno/FaltaUnoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FaltaUnoApplication {
     public static void main(String[] args) {
+        System.out.println("🚀 Falta Uno Backend - v2.0 - Zero-Downtime Deployment");
         SpringApplication.run(FaltaUnoApplication.class, args);
     }
 }
