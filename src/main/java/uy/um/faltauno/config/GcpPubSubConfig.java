0ad8e669aedd009431a9225b80e76f9f9714dc31
augusto-leborgne <leborgneaugusto@gmail.com>
2025-11-12 16:47:36 -0300

    Fix GcpProjectIdProvider bean implementation
    - Use lambda implementation that reads from GOOGLE_CLOUD_PROJECT env var
    - Remove unused DefaultGcpProjectIdProvider import
    - This should resolve the bean not found error during startup

--- src/main/java/uy/um/faltauno/config/GcpPubSubConfig.java
+++ src/main/java/uy/um/faltauno/config/GcpPubSubConfig.java
@@ -4,7 +4,6 @@ import com.google.api.gax.core.CredentialsProvider;
 import com.google.api.gax.core.ExecutorProvider;
 import com.google.api.gax.rpc.TransportChannelProvider;
 import com.google.cloud.spring.core.GcpProjectIdProvider;
-import com.google.cloud.spring.core.DefaultGcpProjectIdProvider;
 import com.google.cloud.spring.pubsub.core.PubSubConfiguration;
 import com.google.cloud.spring.pubsub.core.PubSubTemplate;
 import com.google.cloud.spring.pubsub.support.DefaultPublisherFactory;
@@ -20,7 +19,7 @@ public class GcpPubSubConfig {
 
     @Bean
     public GcpProjectIdProvider gcpProjectIdProvider() {
-        return new DefaultGcpProjectIdProvider();
+        return () -> System.getenv("GOOGLE_CLOUD_PROJECT");
     }
 
     @Bean
