ed480a0c4485d43e17c4b7dcd0e9ff702c8ef743
augusto-leborgne <leborgneaugusto@gmail.com>
2025-11-12 16:40:08 -0300

    Update spring-cloud-gcp-starter-pubsub to 4.0.0 and fix Pub/Sub factory constructors
    - Upgrade spring-cloud-gcp-starter-pubsub from 3.x to 4.0.0
    - Update DefaultPublisherFactory and DefaultSubscriberFactory constructors
      to use GcpProjectIdProvider and PubSubConfiguration for compatibility
    - Fix build issues with Google Cloud Pub/Sub integration

--- src/main/java/uy/um/faltauno/config/GcpPubSubConfig.java
+++ src/main/java/uy/um/faltauno/config/GcpPubSubConfig.java
@@ -1,13 +1,42 @@
 package uy.um.faltauno.config;
 
+import com.google.api.gax.core.CredentialsProvider;
+import com.google.api.gax.core.ExecutorProvider;
+import com.google.api.gax.rpc.TransportChannelProvider;
+import com.google.cloud.spring.core.GcpProjectIdProvider;
+import com.google.cloud.spring.pubsub.core.PubSubConfiguration;
 import com.google.cloud.spring.pubsub.core.PubSubTemplate;
+import com.google.cloud.spring.pubsub.support.DefaultPublisherFactory;
+import com.google.cloud.spring.pubsub.support.DefaultSubscriberFactory;
 import com.google.cloud.spring.pubsub.support.PublisherFactory;
 import com.google.cloud.spring.pubsub.support.SubscriberFactory;
 import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class GcpPubSubConfig {
+
+    @Bean
+    public PubSubConfiguration pubSubConfiguration() {
+        return new PubSubConfiguration();
+    }
+
+    @Bean
+    public PublisherFactory publisherFactory(GcpProjectIdProvider projectIdProvider, CredentialsProvider credentialsProvider, TransportChannelProvider transportChannelProvider) {
+        DefaultPublisherFactory factory = new DefaultPublisherFactory(projectIdProvider);
+        factory.setCredentialsProvider(credentialsProvider);
+        factory.setChannelProvider(transportChannelProvider);
+        return factory;
+    }
+
+    @Bean
+    public SubscriberFactory subscriberFactory(GcpProjectIdProvider projectIdProvider, CredentialsProvider credentialsProvider, ExecutorProvider executorProvider, PubSubConfiguration pubSubConfiguration) {
+        DefaultSubscriberFactory factory = new DefaultSubscriberFactory(projectIdProvider, pubSubConfiguration);
+        factory.setCredentialsProvider(credentialsProvider);
+        factory.setExecutorProvider(executorProvider);
+        return factory;
+    }
 
     @Bean
     public PubSubTemplate pubSubTemplate(PublisherFactory publisherFactory, SubscriberFactory subscriberFactory) {
