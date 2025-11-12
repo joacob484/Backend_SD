8ae93806d6b5bda96eaab145598b1c1d097429aa
augusto-leborgne <leborgneaugusto@gmail.com>
2025-11-12 17:00:22 -0300

    Add missing GCP beans for Pub/Sub configuration
    - Add CredentialsProvider bean using GoogleCredentials.getApplicationDefault()
    - Add ExecutorProvider bean with ScheduledExecutorService
    - Add TransportChannelProvider bean for gRPC communication
    - These beans are required by spring-cloud-gcp 4.0.0 for Pub/Sub factories

--- src/main/java/uy/um/faltauno/config/GcpPubSubConfig.java
+++ src/main/java/uy/um/faltauno/config/GcpPubSubConfig.java
@@ -3,6 +3,9 @@ package uy.um.faltauno.config;
 import com.google.api.gax.core.CredentialsProvider;
 import com.google.api.gax.core.ExecutorProvider;
 import com.google.api.gax.rpc.TransportChannelProvider;
+import com.google.api.gax.grpc.InstantiatingGrpcChannelProvider;
+import com.google.api.gax.core.FixedExecutorProvider;
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.spring.core.GcpProjectIdProvider;
 import com.google.cloud.spring.pubsub.core.PubSubConfiguration;
 import com.google.cloud.spring.pubsub.core.PubSubTemplate;
@@ -10,6 +13,9 @@ import com.google.cloud.spring.pubsub.support.DefaultPublisherFactory;
 import com.google.cloud.spring.pubsub.support.DefaultSubscriberFactory;
 import com.google.cloud.spring.pubsub.support.PublisherFactory;
 import com.google.cloud.spring.pubsub.support.SubscriberFactory;
+import java.io.IOException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 import org.springframework.context.annotation.Bean;
 
 import org.springframework.context.annotation.Configuration;
@@ -20,6 +26,22 @@ public class GcpPubSubConfig {
     @Bean
     public GcpProjectIdProvider gcpProjectIdProvider() {
         return () -> System.getenv("GOOGLE_CLOUD_PROJECT");
+    }
+
+    @Bean
+    public CredentialsProvider credentialsProvider() throws IOException {
+        return () -> GoogleCredentials.getApplicationDefault();
+    }
+
+    @Bean
+    public ExecutorProvider executorProvider() {
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(4);
+        return FixedExecutorProvider.create(executor);
+    }
+
+    @Bean
+    public TransportChannelProvider transportChannelProvider() {
+        return InstantiatingGrpcChannelProvider.newBuilder().build();
     }
 
     @Bean
